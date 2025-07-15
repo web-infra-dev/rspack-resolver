087487d982279a8cd71b1780bf909ced0a98347d
pshu <pishu.spf@antfin.com>
2025-07-15 15:27:03 +0800

    refactor: skip buggy windows prettier check

--- napi/src/tracing.rs
+++ napi/src/tracing.rs
@@ -4,15 +4,15 @@ use tracing_subscriber::filter::Targets;
 use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
-/// To debug `oxc_resolver`:
-/// `OXC_LOG=DEBUG your program`
+/// To debug `rspack_resolver`:
+/// `RSR_LOG=DEBUG your program`
 pub fn init_tracing() {
     static TRACING: OnceLock<()> = OnceLock::new();
     TRACING.get_or_init(|| {
         // Usage without the `regex` feature.
         // <https://github.com/tokio-rs/tracing/issues/1436#issuecomment-918528013>
         tracing_subscriber::registry()
-            .with(std::env::var("OXC_LOG").map_or_else(
+            .with(std::env::var("RSR_LOG").map_or_else(
                 |_| Targets::new(),
                 |env_var| {
                     use std::str::FromStr;
